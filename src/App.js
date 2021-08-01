fc4856ffa3adb5baa612c2385d02ab7ba85a15cf
tobi-oye <tobiloba5000@gmail.com>
2021-08-01 20:48:13 +0100

    [feat] - Landing Page Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import LandingPage from "./LandingPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   // 2. Use at the root of your app
