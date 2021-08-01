b47e0739723e01c0dafd0bd747bf9f9e33782cd8
tobi-oye <tobiloba5000@gmail.com>
2021-08-01 20:45:33 +0100

    [feat] - Landing Page Setup

---
+++ src/LandingPage/index.js
@@ -0,0 +1,17 @@
+import { Text, Flex } from "@chakra-ui/react";
+
+const LandingPage = () => {
+  return (
+    <Flex
+      bgColor="blue.900"
+      justifyContent="center"
+      alignItems="center"
+      h="100vh"
+      color="white"
+    >
+      <Text fontSize="6xl">DocuSign Hackhathon</Text>
+    </Flex>
+  );
+};
+
+export default LandingPage;
