ceb7afa6a7f625c4b40594283e2f864827820576
tobi-oye <tobiloba5000@gmail.com>
2021-08-07 21:20:22 +0100

    [feat] - pet adoption form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import HomePage from "./pages/LandingPage/HomePage.js"
-import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+import HomePage from "./pages/LandingPage/HomePage.js";
+import { BrowserRouter as Route, Router, Switch } from "react-router-dom";
 // import Header from './pages/Header.js'
-import { IconButton, Text, Flex, Box, Button, Input, ListItem, UnorderedList, Heading, Image, Container, Icon, Center } from "@chakra-ui/react";
+import {
+  IconButton,
+  Text,
+  Flex,
+  Box,
+  Button,
+  Input,
+  ListItem,
+  UnorderedList,
+  Heading,
+  Image,
+  Container,
+  Icon,
+  Center,
+} from "@chakra-ui/react";
 import DogPage from "./pages/LandingPage/DogPage.js";
 import AdoptPage from "./pages/LandingPage/AdoptPage.js";
 
 function App() {
   // 2. Use at the root of your app
   return (
-      <ChakraProvider>
-        <Box>
-            <HomePage />
-        </Box>
-        <Box>
-          <AdoptPage />
-        </Box>
-        <Box>
-          <DogPage />
-        </Box>
-      </ChakraProvider>
+    <ChakraProvider>
+      <Box>
+        <HomePage />
+      </Box>
+      <Box>
+        <AdoptPage />
+      </Box>
+      <Box>
+        <DogPage />
+      </Box>
+    </ChakraProvider>
   );
 }
 
