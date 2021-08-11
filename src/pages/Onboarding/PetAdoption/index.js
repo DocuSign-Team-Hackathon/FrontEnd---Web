85ddc6f1f0af586be25e5be0b039c74f7154fffc
Heath Daniel <mr.heath.daniel@gmail.com>
2021-08-10 22:50:39 -0400

    Add rest of questions, add link to PF on submit

--- src/pages/Onboarding/PetAdoption/index.js
+++ src/pages/Onboarding/PetAdoption/index.js
@@ -17,7 +17,7 @@ import {
   NumberDecrementStepper,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
-// import { useState } from "react";
+import { useState } from "react";
 // import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import NumbersInputField from "../../../components/NumberInput";
 import RadioYesOrNo from "../../../components/YesorNoRadio";
@@ -30,10 +30,12 @@ import oaklandanimalservice from "../../../assets/oaklandanimalservice.png";
 const PetAdoptionForm = () => {
   //   const [numPages, setNumPages] = useState(null);
   const {register, handleSubmit} = useForm();
+  const [linkUrl, setLinkUrl] = useState(null);
   //   const [pageNumber, setPageNumber] = useState(1);
   const onSubmit = (data) => {
     const powerFormUrl = getPowerFormUrl(data);
     console.log(powerFormUrl);
+    setLinkUrl(powerFormUrl);
   }
 
   //   function onDocumentLoadSuccess({ numPages }) {
@@ -69,7 +71,7 @@ const PetAdoptionForm = () => {
           <VStack align="start">
             <HStack>
               {" "}
-              <Text>Name</Text> <Input {...register("name")} />
+              <Text>Name</Text> <Input {...register("fullName")} />
             </HStack>
             <HStack>
               {" "}
@@ -197,13 +199,13 @@ const PetAdoptionForm = () => {
           <Text>5</Text>
           <Text>How many people live in the home?</Text>
           <HStack>
-          <NumberInput>
-            <NumberInputField {...register("d_5")} />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
+            <NumberInput>
+              <NumberInputField {...register("d_5")} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
             <Text>Adults</Text>
             <NumberInput>
               <NumberInputField {...register("d_5_a")} />
@@ -224,8 +226,165 @@ const PetAdoptionForm = () => {
             <Input {...register("d_5_c")} />
           </HStack>
         </HStack>
-        <input type="submit" value="submit" />
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>6</Text>
+          <Text>Are there children who visit the home regularly?</Text>
+          <RadioGroup>
+            <HStack>
+              <Radio value="Yes" {...register("d_6_a")}>Yes</Radio>
+              <Radio value="No" {...register("d_6_a")}>No</Radio>
+            </HStack>
+          </RadioGroup>
+          <Text>Ages:</Text>
+          <Input {...register("d_6_b")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>7</Text>
+          <Text>How many other animals do you currently have?</Text>
+          <HStack>
+            <Text>Dogs</Text>
+            <NumberInput>
+              <NumberInputField {...register("d_7_a")} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <Text>Cats</Text>
+            <NumberInput>
+              <NumberInputField {...register("d_7_b")} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <Text>Other</Text>
+            <NumberInput>
+              <NumberInputField {...register("d_7_c")} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <Text>If you have dogs, what breeds?</Text>
+            <Input {...register("d_7_d")} />
+          </HStack>
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>8</Text>
+          <Text>Have you had dogs in the past?</Text>
+          <RadioGroup>
+            <HStack>
+              <Radio value="Yes" {...register("d_8_a")}>Yes</Radio>
+              <Radio value="No" {...register("d_8_a")}>No</Radio>
+            </HStack>
+          </RadioGroup>
+          <Text>How many?</Text>
+          <Input {...register("d_8_b")} />
+          <Text>If yes, what happened to them?</Text>
+          <Input {...register("d_8_c")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>9</Text>
+          <Text>Where will the dog stay during the day? Describe:</Text>
+          <Input {...register("d_9_1")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>10</Text>
+          <Text>Where will the dog stay at night? Describe:</Text>
+          <Input {...register("d_10_1")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>11</Text>
+          <Text>On average, how long will the dog be alone each day?</Text>
+          <Input {...register("d_11")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>12</Text>
+          <Text>Where will the dog be kept when it is alone?</Text>
+          <Input {...register("d_12")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>13</Text>
+          <Text>Do you have a yard?</Text>
+          <RadioGroup>
+            <HStack>
+              <Radio value="Yes" {...register("d_13_a")}>Yes</Radio>
+              <Radio value="No" {...register("d_13_a")}>No</Radio>
+            </HStack>
+          </RadioGroup>
+          <Text>How high is the fence around yard?</Text>
+          <Input {...register("d_13_b")} />
+          <Text>Locked?</Text>
+          <Checkbox {...register("d_13_c")} value="Yes">Yes</Checkbox>
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>14</Text>
+          <Text>How will you exercise your dog?</Text>
+          <Input {...register("d_14")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>15</Text>
+          <Text>Do you understand that your new dog may show unexpected and unwanted behaviors, such as destructive chewing, loud/excessive barking, dog aggression, separation anxiety and even signs of human aggression, when he/she becomes comfortable in your home?</Text>
+          <RadioGroup>
+            <HStack>
+              <Radio value="Yes" {...register("d_15")}>Yes</Radio>
+              <Radio value="No" {...register("d_15")}>No</Radio>
+            </HStack>
+          </RadioGroup>
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>16</Text>
+          <Text>How will you handle behavior problems when they appear?</Text>
+          <Input {...register("d_16_1")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>17</Text>
+          <Text>If you will use a dog trainer or attend dog training classes, list their information here:</Text>
+          <Input {...register("d_17")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>18</Text>
+          <Text>This dog may not be housebroken. How will you housetrain the dog?</Text>
+          <Input {...register("d_18")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>19</Text>
+          <Text>What is the name of the veterinarian to whom you will take your dog?</Text>
+          <Input {...register("d_19")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>20</Text>
+          <Text>What would you do with the dog if something happened and you could no longer keep the dog?</Text>
+          <Input {...register("d_20")} />
+        </HStack>
+
+        <HStack w="100%" whiteSpace="nowrap">
+          <Text>21</Text>
+          <Text>How did you hear about Oakland Animal Services?</Text>
+          <Input {...register("d_21")} />
+        </HStack>
+
+        <input type="submit" value="Submit" />
       </form>
+      {linkUrl ? (
+        <a href={linkUrl}>Go to Form</a>
+      ) : null}
     </VStack>
   );
 };
