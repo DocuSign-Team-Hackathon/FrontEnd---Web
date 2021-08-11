679cbf3e405b1a3e7f1d75e5309e9f50e04424b4
Heath Daniel <mr.heath.daniel@gmail.com>
2021-08-10 22:49:55 -0400

    Handle null values on form

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,9 @@
-const replaceSpacesWithPluses = (s) => s.split(" ").join("+");
+const replaceSpacesWithPluses = (s) => {
+  if (s)
+    return s.split(" ").join("+");
+  else
+    return s;
+};
 
 export const getPowerFormUrl = (formData) => {
   const URL_START = 'https://demo.docusign.net/Member/PowerFormSigning.aspx?PowerFormId=cc332d81-dd07-4f59-8938-8e6e42623cd0&env=demo&acct=70effc48-d52c-4e4d-adb8-828bf793ebca&v=2&';
